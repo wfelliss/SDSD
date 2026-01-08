cc2200251abfde01cd057a7ea394ef86a3025e70
WES3 <WES3@3ds.com>
2026-01-08 23:43:47 +0000

    added vite to env variables

--- apps/frontend/app/api/client.ts
+++ apps/frontend/app/api/client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const apiClient = axios.create({
-  baseURL: (import.meta.env.API_BASE_URL || "http://localhost:3001") + "/api",
+  baseURL: (import.meta.env.VITE_API_BASE_URL || "http://localhost:3001") + "/api",
 })
