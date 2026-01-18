a7c9e93fc2661fd8f44771554c857fc44da8b85a
WES3 <WES3@3ds.com>
2026-01-18 11:27:48 +0000

    moved all db stuff ot the database folder, updated relevant scripts to work for deployment

--- apps/backend/drizzle/0000_chief_hammerhead.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"bio" text,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
--- apps/backend/drizzle/0002_common_dakota_north.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "runs" ADD COLUMN "title" varchar(255);
